7c6651d3efba68794368595907545fd0b6b0b111
Luis <58019574+ludev84@users.noreply.github.com>
2025-06-30 20:14:51 -0600

    1st specification done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
+
+import markdown2
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def entry(request, entry):
+    entry_md = util.get_entry(entry)
+    if entry_md:
+        entry_md = markdown2.markdown(entry_md)
+    return render(
+        request,
+        "encyclopedia/entry.html",
+        {"entry": entry_md, "title": entry},
+    )
